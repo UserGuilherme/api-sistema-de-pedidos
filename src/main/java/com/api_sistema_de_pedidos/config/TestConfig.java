851426c4f5880b5f6297c77b6f10edd693342259
Guilherme de Araújo <164342779+UserGuilherme@users.noreply.github.com>
2024-07-29 23:17:42 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/api_sistema_de_pedidos/config/TestConfig.java
+++ src/main/java/com/api_sistema_de_pedidos/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.api_sistema_de_pedidos.config;
 
 import com.api_sistema_de_pedidos.entities.Category;
 import com.api_sistema_de_pedidos.entities.Order;
+import com.api_sistema_de_pedidos.entities.Product;
 import com.api_sistema_de_pedidos.entities.User;
 import com.api_sistema_de_pedidos.entities.enums.OrderStatus;
 import com.api_sistema_de_pedidos.repositories.CategoryRepository;
@@ -32,16 +33,8 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private ProductRepository productRepository;
 
-
     @Override
     public void run(String... args) throws Exception {
-
-        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
-
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
         Category cat1 = new Category(null, "Electronics");
         Category cat2 = new Category(null, "Books");
@@ -55,6 +48,22 @@ public class TestConfig implements CommandLineRunner {
 
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+        p1.getCategories().add(cat2);
+        p2.getCategories().add(cat1);
+        p2.getCategories().add(cat3);
+        p3.getCategories().add(cat3);
+        p4.getCategories().add(cat3);
+        p5.getCategories().add(cat2);
+
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
