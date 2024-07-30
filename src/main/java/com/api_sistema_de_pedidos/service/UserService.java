25f0fcc947d4c3b0bd5b2d03c55d7750f6dedc97
Guilherme de Araújo <164342779+UserGuilherme@users.noreply.github.com>
2024-07-29 22:14:46 -0300

    Service layer, component registration

---
+++ src/main/java/com/api_sistema_de_pedidos/service/UserService.java
@@ -0,0 +1,25 @@
+package com.api_sistema_de_pedidos.service;
+
+import com.api_sistema_de_pedidos.entities.User;
+import com.api_sistema_de_pedidos.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
