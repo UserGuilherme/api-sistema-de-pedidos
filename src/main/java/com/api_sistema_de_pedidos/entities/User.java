11f8249e60ccb7966610cc14b33826ad101d922b
Guilherme de Araújo <164342779+UserGuilherme@users.noreply.github.com>
2024-07-29 21:42:35 -0300

    H2 database, test profile, JPA

--- src/main/java/com/api_sistema_de_pedidos/entities/User.java
+++ src/main/java/com/api_sistema_de_pedidos/entities/User.java
@@ -1,10 +1,15 @@
 package com.api_sistema_de_pedidos.entities;
 
-import java.io.Serializable;
+import jakarta.persistence.*;
 
+import java.io.Serializable;
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
