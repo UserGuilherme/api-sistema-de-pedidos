e14bb921ba8ad3df3bb85633eb4fc7bf923329f6
Guilherme de Araújo <164342779+UserGuilherme@users.noreply.github.com>
2024-09-13 06:13:33 -0300

    Exception handling - update end delete

--- src/main/java/com/api_sistema_de_pedidos/services/UserService.java
+++ src/main/java/com/api_sistema_de_pedidos/services/UserService.java
@@ -2,8 +2,12 @@ package com.api_sistema_de_pedidos.services;
 
 import com.api_sistema_de_pedidos.entities.User;
 import com.api_sistema_de_pedidos.repositories.UserRepository;
+import com.api_sistema_de_pedidos.resources.exceptions.DatabaseException;
 import com.api_sistema_de_pedidos.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -27,12 +31,23 @@ public class UserService {
         return repository.save(obj);
     }
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
+
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
