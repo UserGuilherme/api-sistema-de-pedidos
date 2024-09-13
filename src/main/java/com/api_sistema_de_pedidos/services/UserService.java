fb8648b108a3f637c83e92d022e366ff57d114ca
Guilherme de Araújo <164342779+UserGuilherme@users.noreply.github.com>
2024-09-13 05:49:48 -0300

    Exception handling - findById

--- src/main/java/com/api_sistema_de_pedidos/services/UserService.java
+++ src/main/java/com/api_sistema_de_pedidos/services/UserService.java
@@ -2,6 +2,7 @@ package com.api_sistema_de_pedidos.services;
 
 import com.api_sistema_de_pedidos.entities.User;
 import com.api_sistema_de_pedidos.repositories.UserRepository;
+import com.api_sistema_de_pedidos.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
     public User insert(User obj) {
         return repository.save(obj);
